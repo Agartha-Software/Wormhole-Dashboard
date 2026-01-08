4dfbbb4c12a1905d14815e86e8b4c5ec5245fdf9
Axel Denis <axel.denis@outlook.fr>
2026-01-08 17:25:28 +0100

    draft: proof of concept chart

--- src/components/pages/NetworkPage.tsx
+++ src/components/pages/NetworkPage.tsx
@@ -4,6 +4,11 @@ import type { Network } from "../../actions/get/networks";
 import H2 from "../text/H2";
 import Status from "../dynamic/Status";
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 export default function ({ name }: { name: string }) {
     const [network, setNetwork] = useState<Network | null | undefined>(null);
     useEffect(() => {
@@ -14,9 +19,30 @@ export default function ({ name }: { name: string }) {
     }, []);
 
     return (
-        <div class="flex items-center gap-5 h-6">
-            <Status status={network?.status} />
-            <H2>{name}</H2>
-        </div>
+        <>
+            <div class="flex items-center gap-5 h-6">
+                <Status status={network?.status} />
+                <H2>{name}</H2>
+            </div>
+            <div className="w-full h-full">
+                <Doughnut data={{
+                    labels: [
+                        'Red',
+                        'Blue',
+                        'Yellow'
+                    ],
+                    datasets: [{
+                        label: 'My First Dataset',
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)'
+                        ],
+                        hoverOffset: 4
+                    }]
+                }} />
+            </div>
+        </>
     )
 }
