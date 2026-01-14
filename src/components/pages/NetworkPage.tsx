5e0217c75fb7aa83fa9bfaae59c50ba99de40c3e
Axel Denis <axel.denis@epitech.eu>
2026-01-14 16:43:28 +0100

    draft: draft migrating to new api

--- src/components/pages/NetworkPage.tsx
+++ src/components/pages/NetworkPage.tsx
@@ -29,23 +29,24 @@ export default function ({ name }: { name: string }) {
     return (
         <>
             <div class="flex items-center gap-5 h-6">
-                <Status status={validState(network)?.status} />
+                <Status status="online" />
                 <H2>{name}</H2>
             </div>
             <div className="w-full h-full">
                 <Doughnut data={{
                     labels: [
-                        'Red',
-                        'Blue',
-                        'Yellow'
+                        'Used space',
+                        'Remaining space',
                     ],
                     datasets: [{
-                        label: 'My First Dataset',
-                        data: [300, 50, 100],
+                        label: 'Disk space',
+                        data: [
+                            validState(network)?.disk_space.total_size - validState(network)?.disk_space.free_size,
+                            validState(network)?.disk_space.free_size
+                        ],
                         backgroundColor: [
                             'rgb(255, 99, 132)',
                             'rgb(54, 162, 235)',
-                            'rgb(255, 205, 86)'
                         ],
                         hoverOffset: 4
                     }]
